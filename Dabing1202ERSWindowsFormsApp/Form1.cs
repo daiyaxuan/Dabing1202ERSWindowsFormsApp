54da1709cc998d78d6a5e12f8b1fa02c6632e5ee
戴亚轩 <7208030959@qq.com>
2023-05-11 16:42:01 +0800

    添加项目文件。

---
+++ Dabing1202ERSWindowsFormsApp/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dabing1202ERSWindowsFormsApp
+{
+    public partial class EmployeeRecordsForm : Form
+    {
+        private TreeNode tvRootNode;
+        public EmployeeRecordsForm()
+        {
+            InitializeComponent();
+            PopulateTreeView();
+            InitalizeListControl();
+
+        }
+
+        private void statusBar1_PanelClick(object sender, StatusBarPanelClickEventArgs e)
+        {
+
+        }
+
+        private void EmployeeRecordsForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void PopulateTreeView()
+        {
+            statusBarPanel1.Tag = "Refreshing Employee Code.Please wait...";
+            this.Cursor = Cursors.WaitCursor;
+            treeView1.Nodes.Clear();
+            tvRootNode = new TreeNode("Employee Rocords");
+            this.Cursor = Cursors.Default;
+            treeView1.Nodes.Add(tvRootNode);
+
+            TreeNodeCollection nodeCollection = tvRootNode.Nodes;
+            XmlTextReader reader = new XmlTextReader("C:\\Users\\14357\\source\\repos\\Dabing1202ERSWindowsFormsApp\\Dabing1202ERSWindowsFormsApp\\EmpRec.xml");
+            reader.MoveToElement();
+            try
+            {
+                while(reader.Read())
+                {
+                    if (reader.HasAttributes && reader.NodeType == XmlNodeType.Element)
+                    {
+                        reader.MoveToElement();
+                        reader.MoveToElement();
+
+                        reader.MoveToAttribute("Id");
+                        String strVal = reader.Value;
+
+                        reader.Read();
+                        reader.Read();
+                        if (reader.Name == "Dept")
+                        {
+                            reader.Read();
+                        }
+                        TreeNode EcodeNode = new TreeNode(strVal);
+                        nodeCollection.Add(EcodeNode);
+
+
+
+
+                    }
+                }
+                statusBarPanel1.Tag = "Click on an emplyoee code to see their record.";
+            }
+            catch(XmlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        protected void InitalizeListControl()
+        {
+            listView1.Clear();
+            listView1.Columns.Add("Employee Name", 255, HorizontalAlignment.Left);
+            listView1.Columns.Add("Date of Join", 70, HorizontalAlignment.Right);
+            listView1.Columns.Add("Gread", 105, HorizontalAlignment.Left);
+            listView1.Columns.Add("Salary", 105, HorizontalAlignment.Left);
+
+        }
+        protected void PopulateListView(TreeNode crrNode)
+        {
+            InitalizeListControl();
+            XmlTextReader listRead = new XmlTextReader("");
+            listRead.MoveToElement();
+            while (listRead.Read())
+            {
+                String strNodeName;
+                String strNodePath;
+                String name;
+                String gread;
+                String doj;
+                String sal;
+                String[] strItemsArr = new string[4];
+                listRead.MoveToFirstAttribute();
+                strNodeName = listRead.Value;
+                strNodePath = crrNode.FullPath.Remove(0, 17);
+                if (strNodePath == strNodeName)
+                {
+                    ListViewItem lvi;
+
+                    listRead.MoveToNextAttribute();
+                    name = listRead.Value;
+                    lvi = listView1.Items.Add(listRead.Value);
+
+                    listRead.Read();
+                    listRead.Read();
+
+                    listRead.MoveToFirstAttribute();
+                    doj = listRead.Value;
+                    lvi.SubItems.Add(doj);
+
+                    listRead.MoveToNextAttribute();
+                    gread = listRead.Value;
+                    lvi.SubItems.Add(gread);
+
+                    listRead.MoveToFirstAttribute();
+                    sal = listRead.Value;
+                    lvi.SubItems.Add(sal);
+
+                    listRead.MoveToNextAttribute();
+                    listRead.MoveToElement();
+                    listRead.ReadString();
+
+                }
+            }
+        }
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeNode currNode = e.Node;
+            if (tvRootNode == currNode)
+            {
+                statusBarPanel1.Text = "Double Click the Employee Records";
+                return;
+            }
+            else
+            {
+                statusBarPanel1.Text = "Click an Employee code to view individual record";
+            }
+            PopulateListView(currNode);
+        }
+    }
+}
